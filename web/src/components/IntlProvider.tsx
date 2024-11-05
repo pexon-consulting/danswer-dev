5a2e8a7a29f30372716da110431dc865b1682f23
Just Lenz <just.lenz@hotmail.de>
2024-11-05 10:13:41 +0100

    config adaptions

--- web/src/components/IntlProvider.tsx
+++ web/src/components/IntlProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { I18nextProvider } from "react-i18next";
 import initTranslations from "@/app/i18n";
-import { createInstance, Resource, TFunction } from "i18next";
+import { createInstance, Resource } from "i18next";
+
 export default function IntlProvider({
   children,
   locale,
@@ -15,6 +16,16 @@ export default function IntlProvider({
   resources: Resource | undefined;
 }) {
   const i18n = createInstance();
-  initTranslations(locale, namespaces, i18n, resources);
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await initTranslations(locale, namespaces, i18n, resources);
+      setIsInitialized(true);
+    })();
+  }, [locale, namespaces, resources]);
+
+  if (!isInitialized) return null; // Render nothing until i18n is initialized
+
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
